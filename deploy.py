6e380847b696abdc082ac4c3992379caa33aa0ce
Gritter <pedro12345g@gmail.com>
2018-09-17 14:37:46 +0100

    INIT: token-qr inicial commit

---
+++ deploy.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from flask import render_template
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return render_template('token-qr-template.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Do not leave on on real situations
