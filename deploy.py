e246b52298f3d509f82a9158afb86e99c2795c5b
Pedro Gritter <pedro12345g@gmail.com>
2018-09-18 15:54:45 +0100

    Modified - deploy.py

--- deploy.py
+++ deploy.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from flask import render_template
-from flask_assets import Environment, Bundle
 
 app = Flask(__name__, static_url_path='')
 
@@ -12,17 +11,3 @@ def root():
 if __name__ == '__main__':
     app.run(debug=True),  # Do not leave on on real situations
     app.debug = True
-
-    assets = Environment(app)
-    assets.url = app.static_url_path
-
-    scss = Bundle('glitch.scss', filters='pyscss', output='scss_all.css')
-
-    assets.config['SECRET_KEY'] = 'secret!'
-    assets.config['PYSCSS_LOAD_PATHS'] = assets.load_path
-    assets.config['PYSCSS_STATIC_URL'] = assets.url
-    assets.config['PYSCSS_STATIC_ROOT'] = assets.directory
-    assets.config['PYSCSS_ASSETS_URL'] = assets.url
-    assets.config['PYSCSS_ASSETS_ROOT'] = assets.directory
-
-    assets.register('scss_all', scss)
